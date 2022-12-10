1f51149a971a61556a1fa6269093d5d4c4ed388c
guillermo rodriguez <guillermordgz.96@gmail.com>
2022-12-10 12:15:07 -0800

    event button

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// ask location
+
 const successCallback = (position) => {
     console.log(position);
   };
@@ -8,9 +10,27 @@ const successCallback = (position) => {
   
   navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 
+// leaflet map
+
   var map = L.map('map').setView([33.765593, -118.192727], 16);
 
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+// YOU ARE HERE
+var marker = L.marker([33.766612, -118.183518]).addTo(map)
+.bindPopup('<p1><b>You are here</b><br></p1>')
+.openPopup()
+// YOU ARE HERE
+
+//
+
+// submit button
+document.getElementById('submit').addEventListener('click', async (event) => {
+    event.preventDefault()
+    let business = document.getElementById('business').value
+    console.log(business)
+})
